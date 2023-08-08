c8dfba665ee583684e1feed790f889a9b20dc742
salutcmoi9 <127397749+salutcmoi9@users.noreply.github.com>
2023-08-08 13:49:44 +0200

    algo trading price action

---
+++ README.m
@@ -0,0 +1,9 @@
+- tracer support et resistance avec minimum 5 point de touche
+- tracer trendline avec minimum 3 point de touche 
+- attendre la cassure de cette trendline 
+- attendre minimum 2 pull back
+- attendre une meche de rejet sur resistance ou support
+- puis voir si il y'a une divergence
+- entrer en position 
+
+
